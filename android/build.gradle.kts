5b6aa8788aa81123ee00905e91fefa166d697a01
Ehsan NAEIMI <ehsan.system67@gmail.com>
2025-10-22 12:30:02 +0330

    Add Flutter project files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,6 +23,9 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+    kotlin("android") version "1.9.10" apply false
 }
+
+
