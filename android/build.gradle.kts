6e46a58c1f60d9fe75189768c20ffc3527fc674c
Ehsan NAEIMI <ehsan.system67@gmail.com>
2025-10-22 13:52:40 +0330

    Remove AGP version from root build.gradle.kts for Codemagic

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,5 @@
-plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
-    kotlin("android") version "1.9.10" apply false
-}
+// حذف بخش plugins از root build.gradle.kts
+// فقط بخش allprojects را نگه دارید
 
 allprojects {
     repositories {
@@ -10,24 +7,3 @@ allprojects {
         mavenCentral()
     }
 }
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-
-
-
